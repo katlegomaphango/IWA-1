d77aeb7e690867e70adc313fa3eabf72bb010fbf
Katlego Maphango <mphangokatlego08@gmail.com>
2023-03-27 15:51:49 +0200

    Added event listernes

--- scripts.js
+++ scripts.js
@@ -1,7 +1,9 @@
 const MAX_NUMBER = 15;
 const MIN_NUMBER = -5;
 
-
 const number = document.querySelector('[data-key="number"]');
 const add = document.querySelector('[data-key="add"]');
 const subtract = document.querySelector('[data-key="subtract"]');
+
+subtract.addEventListener('click', subtractHandler);
+add.addEventListener('click', addHandler);
