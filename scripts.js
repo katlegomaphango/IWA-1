4092057bd531fc3c250e796db7084ee663fa4212
Katlego Maphango <mphangokatlego08@gmail.com>
2023-03-27 15:50:37 +0200

    Added data-key attributes to use in my js

---
+++ scripts.js
@@ -0,0 +1,7 @@
+const MAX_NUMBER = 15;
+const MIN_NUMBER = -5;
+
+
+const number = document.querySelector('[data-key="number"]');
+const add = document.querySelector('[data-key="add"]');
+const subtract = document.querySelector('[data-key="subtract"]');
